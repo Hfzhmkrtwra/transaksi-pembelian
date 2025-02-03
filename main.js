68d0a3620d7a52c4d664f27c692416579012e18a
Hfzhmkrtwra <hafizahkartawiria701@gmail.com>
2025-02-03 09:06:46 +0700

    Menambah fungsi konfigurasi dan fungsi daftar barang

--- main.js
+++ main.js
@@ -1,1 +1,46 @@
-console.log('Hello World!');
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js'
+import {
+  getFirestore,
+  collection,
+  doc,
+  getDocs,
+  getDoc,
+  addDoc,
+  deleteDoc,
+  updateDoc,
+  query,
+  orderBy
+} from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js'
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDGYnq4VKq-YGu4RbfoI_ZHez9fishYjZo",
+  authDomain: "insan-cemerlang-afd2f.firebaseapp.com",
+  projectId: "insan-cemerlang-afd2f",
+  storageBucket: "insan-cemerlang-afd2f.appspot.com",
+  messagingSenderId: "686649580589",
+  appId: "1:686649580589:web:61374bbbd68adb604eaca4",
+  measurementId: "G-LNZTQBCE26"
+};
+
+//inisialisasi firebase
+const aplikasi = initializeApp(firebaseConfig)
+const basisdata = getFirestore(aplikasi)
+
+//fungsi ambil daftar barang 
+export async function ambilDaftarBarang() {
+  const refDokumen = collection(basisdata, "inventory");
+  const kueri = query(refDokumen, orderBy("item"));
+  const cuplikanKueri = await getDocs(kueri);
+
+  let hasilKueri = [];
+  cuplikanKueri.forEach((dokumen) => {
+    hasilKueri.push({
+      id: dokumen.id,
+      item: dokumen.data().item,
+      jumlah: dokumen.data().jumlah,
+      harga: dokumen.data().harga
+    })
+  })
+
+  return hasilKueri;
+}
